57651827f3ffafcc745bff40964acd2df0431e02
Batoul <batouldaou@gmail.com>
2024-06-11 08:03:59 +0200

    added category to transaction forms

--- forms.py
+++ forms.py
@@ -9,14 +9,15 @@ from wtforms.validators import InputRequired, Length
 
 class TransactionForms(FlaskForm):
     amount = DecimalField('Amount')
-    #cateogry = SelectField('Category', [InputRequired()], choices=[])
+    category = SelectField('Category', [InputRequired()], choices=[])
     type = SelectField('Type', [InputRequired()], choices=[])
     submit = SubmitField('Add Transaction')
     
     
-    def __init__(self, *args):
-        super(TransactionForms, self).__init__(*args)
-        #fself.category.choices = [category for category in args]
+    def __init__(self, categories=None, *args):
+        super(TransactionForms, self).__init__(*args)        
         self.type.choices = ['Income', 'Expense']
+        if categories:
+            self.category.choices = [category["category_name"] for category in categories]
     
     
